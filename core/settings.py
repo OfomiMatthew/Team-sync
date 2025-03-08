cf6a425672c9551f2e804f01ee2e84e89d7d8946
MatthewOfomi <ofomimatthew7@gmail.com>
2025-03-08 20:40:33 +0100

    feat: cors headers added

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['.vercel.app']
 
 
-
+# ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -39,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     "corsheaders",
     'TeamSync'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
